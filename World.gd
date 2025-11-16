c968d2009f854709f6d258b236a10976e2079232
valfrom <valfrom@gmail.com>
2025-11-16 20:05:14 +0100

    Ensure world assigns planet reference

--- World.gd
+++ World.gd
@@ -7,12 +7,16 @@ var player: Player
 
 func _ready() -> void:
         if player_scene == null:
-                player_scene = load("res://player.tscn")
+                player_scene = load("res://player/player.tscn")
+
+        if planet == null:
+                planet = get_node_or_null(^"Planet")
 
         player = player_scene.instantiate() as Player
-        add_child(player)
-
         if planet != null:
                 player.planet = planet
+        else:
+                push_warning("World has no planet assigned. Player will use default gravity.")
 
+        add_child(player)
         player.global_position = Vector3(0.0, 110.0, 0.0)
